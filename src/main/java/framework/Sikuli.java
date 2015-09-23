7ae0cee74a5317c470006e4050510ec3d3a7b51a
Oleksandr Yaroslavtsev <alexdimukr@gmail.com>
2015-09-23 18:15:00 +0300

    added blobs of: - verifyImageOnScreen - clickOnImage

--- src/main/java/framework/Sikuli.java
+++ src/main/java/framework/Sikuli.java
@@ -1,5 +1,24 @@
 package main.java.framework;
 
+
+import org.sikuli.*;
+
+	
 public class Sikuli {
+	private Screen screen;
+	
+	// verify that image is visible in browser
+	public boolean verifyImageOnScreen() {
+		
+	}
+	
+	// click on image center
+	public void clickOnImage() {
+		
+	}
+	
+	
+	// get coordinates of image
+	 
 	
 }
